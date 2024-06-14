4eb9a00c45f4cec7f67f9ada5f0fb5d6092a66d1
nahuescudaletti <nahueby17@gmail.com>
2024-06-14 12:50:13 -0300

    change favicon

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b635dc8ccef03d07"; }
+/******/ 		__webpack_require__.h = function() { return "d1e53ebbd30639b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
