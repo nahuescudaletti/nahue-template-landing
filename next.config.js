e76769664bbaa212134100178a18f2da80eb63ac
nahuescudaletti <nahueby17@gmail.com>
2024-06-14 00:03:36 -0300

    test1nextstep solved

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'; // Aquí debes usar === en lugar de =
 const nextConfig = {
-    basePath: isProd ? '/nahue-template-landing' : '',
     output: 'export',
     distDir:'dist',
     images:{
